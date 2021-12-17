c007502c62982c2c5e7eacb81e4c7e92142bbe1e
cjgochanour <cjgochanour@gmail.com>
2021-12-17 14:09:27 -0600

    corrected API URL

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,4 +1,4 @@
-const API = "sink-repair-dev22.us-east-1.elasticbeanstalk.com";
+const API = "sink-repair-dev22.us-east-1.elasticbeanstalk.com/api";
 export const mainContainer = document.querySelector("#container");
 
 // if the request id is found in the completions id, put it last
